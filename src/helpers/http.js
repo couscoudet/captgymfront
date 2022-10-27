36c27dfce6becd9c37cc0d9fddf56d763b96b705
yannick SOULTANE <ysoultane@gmail.com>
2022-10-27 19:51:26 +0200

    add .env and environment variable for API

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'http://captgym.local/api'
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 http.defaults.headers.post['Content-Type'] = 'application/json';
