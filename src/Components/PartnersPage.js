8865469fe41faa5371eefc5edc2aec9217954300
yannick SOULTANE <ysoultane@gmail.com>
2022-10-29 15:59:09 +0200

    add perms function to addmodal

--- src/Components/PartnersPage.js
+++ src/Components/PartnersPage.js
@@ -7,7 +7,7 @@ import MyTable from './MyTable'
 import AddPartnerModal from './AddPartnerModal';
 
 export default function PartnersPage() {
-    const submitPartner = (name, address, postalCode, city, country, phone, description, logo, website, active) => {
+    const submitPartner = (name, address, postalCode, city, country, phone, description, logo, website, active, modulePerms) => {
       let partnerData = {
         name: name,
         address: address,
@@ -18,28 +18,20 @@ export default function PartnersPage() {
         description: description,
         logo: logo,
         website: website,
-        active: active
+        active: active,
+        modulePerms: modulePerms
       };
       console.log(JSON.stringify(partnerData));
-      http.post('/partners',JSON.stringify(partnerData));
-      // http.post('/partners', {
-      //   name: name,
-      //   address: address,
-      //   postalCode: postalCode, 
-      //   city: city,
-      //   country: country,
-      //   phone: phone, 
-      //   description: description,
-      //   logo: logo,
-      //   website: website,
-      //   active: active
-      // })
-      // .then(function (response) {
-      //   console.log(response);
-      // })
-      // .catch(function (error) {
-      //   console.log(error);
-      // });
+      http.post('/partners',JSON.stringify(partnerData))
+      .then(function (response) {
+          console.log(response);
+            http.get('/partners')
+            .then(response => setData(response.data["hydra:member"]))
+            .catch(error => console.log(error));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
       };
 
     const [data, setData] = useState('');
@@ -58,7 +50,7 @@ export default function PartnersPage() {
     <div>
         <div>{data ? data[0].name : 'chargement'}</div>
         <Header />
-        <AddPartnerModal submitPartner={(name, address, postalCode, city, country, phone, description, logo, website, active) => submitPartner(name, address, postalCode, city, country, phone, description, logo, website, active)}/>
+        <AddPartnerModal submitPartner={(name, address, postalCode, city, country, phone, description, logo, website, active, modulePerms) => submitPartner(name, address, postalCode, city, country, phone, description, logo, website, active, modulePerms)}/>
         <MyTable parentData={data ? data : ''} />
     </div>
   )
